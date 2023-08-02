20d7eafd566feb7068a7a2b9d10d9b74d7acd68e
Amrey Mathurin <mamrey@upenn.edu>
2023-08-02 12:05:58 -0400

    Update AlmaApi::Client specs - update method names - test exception handling - use array of bib_ids in tests

--- spec/services/alma_api/client_spec.rb
+++ spec/services/alma_api/client_spec.rb
@@ -4,46 +4,50 @@ describe AlmaApi::Client do
 
   describe '.bibs' do
     let(:client) { described_class.new }
-    let(:bib_id) { 'sample_bib' }
+    let(:bib_ids) { %w[sample_bib another_bib] }
     let(:marc_xml) { 'test' }
 
     context 'with a successful request' do
 
-      before { stub_alma_api_bib_request(bib_id, marc_xml) }
+      before { stub_alma_api_bibs_request(bib_ids, marc_xml) }
 
       it 'returns the response body' do
-        expect(client.bibs(bib_id)).to eq ('test')
+        expect(client.bibs(bib_ids)).to eq ('test')
       end
     end
 
-    context 'with response with status 4xx-5xx' do
-      before { stub_alma_api_bib_http_error(bib_id) }
+    context 'when receiving 4xx-5xx response with alma api error in payload' do
+      before { stub_alma_api_bibs_http_error(bib_ids, alma_api_bibs_mock_error) }
 
       it 'raises the expected error message' do
-        allow(client).to receive(:alma_bibs_error).and_return(error_code: '401652', error_message: 'General Error')
         expect {
-          client.bibs(bib_id)
-        }.to raise_error(AlmaApi::Client::Error, '401652: General Error')
+          client.bibs(bib_ids)
+        }.to raise_error(AlmaApi::Client::Error,
+                         'Could not retrieve bibs requested. 401652 General Error - An error has occurred')
+      end
+    end
+
+    context 'when receiving error without alma api in payload' do
+      before { stub_alma_api_bibs_http_error(bib_ids, nil) }
+
+      it 'raises the expected error message' do
+        expect {
+          client.bibs(bib_ids)
+        }.to raise_error(AlmaApi::Client::Error, 'Could not retrieve bibs requested.')
       end
     end
 
     context 'when mmsids exceeds maximum allowed' do
-      let(:bib_id) { (0..50).to_a }
+      let(:bib_ids) { (0..50).to_a }
 
-      before { stub_alma_api_bib_request(bib_id, marc_xml) }
+      before { stub_alma_api_bibs_request(bib_ids, marc_xml) }
 
       it 'raises error' do
         expect {
-          client.bibs(bib_id)
+          client.bibs(bib_ids)
         }.to raise_error(AlmaApi::Client::Error,
                          'Too many mms ids provided, exceeds the maximum allowed 50.')
       end
-    end
-  end
-
-  describe '.alma_bibs_error' do
-    it 'returns expected value' do
-      skip
     end
   end
 end
