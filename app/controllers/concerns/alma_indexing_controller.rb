8c6784299345483b48b377c7f8e5c8fb90a33f5d
Patrick Perkins <lilblizzard97@gmail.com>
2023-08-02 13:20:43 -0400

    change notice presentation

--- app/controllers/concerns/alma_indexing_controller.rb
+++ app/controllers/concerns/alma_indexing_controller.rb
@@ -8,11 +8,10 @@ class AlmaIndexingController < ApplicationController
     ids.length <= AlmaApi::Client::MAX_BIBS_GET ? puts("success: #{ids.inspect}") : puts('failure!')
     if ids.length <= AlmaApi::Client::MAX_BIBS_GET
       # send IDs to alma
-      flash.notice = "Success!"
+      redirect_to root_path, notice: 'Success!'
     else
-      flash.alert = "Number of MMS IDs exceeds the limit (#{AlmaApi::Client::MAX_BIBS_GET})"
+      redirect_to root_path, alert: "Number of MMS IDs exceeds the limit (#{AlmaApi::Client::MAX_BIBS_GET})"
     end
-    redirect_to root_path
   end
 end
 
