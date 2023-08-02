a26cfc0034ed3f64791f077a96afa470472c9310
Amrey Mathurin <mamrey@upenn.edu>
2023-08-02 12:00:31 -0400

    Allow alma api stubs to receive array of bib ids - update method names to better reflect bibs api

--- spec/support/alma_api_mocks.rb
+++ spec/support/alma_api_mocks.rb
@@ -2,21 +2,23 @@
 
 # Contains helper methods to stub Alma Api requests
 module AlmaApiMocks
-  # @param [String] bib_ids comma separated mms ids of alma bibliographic record
+  # @param [Array<String>] bib_ids list of mms ids of alma bibliographic record
   # @param [String] alma_marc_xml MARC XML of given record
   # @param [String] alma_api_key key to authorize alma api request
   # @return [WebMock::RequestStub]
   def stub_alma_api_request(bib_ids, alma_marc_xml, alma_api_key = '')
+    bib_ids = Array.wrap(bib_ids).join(',')
     stub_request(
       :get,
       "https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/?mms_id=#{bib_ids}&expand=p_avail&apikey=#{alma_api_key}"
     ).to_return(body: alma_marc_xml)
   end
 
-  # @param [String] bib_ids comma separated mms id of alma bibliographic record
+  # @param [String] bib_ids list of mms ids of alma bibliographic record
   # @param [String] alma_marc_xml MARC XML of given record
   # @return [WebMock::RequestStub]
-  def stub_alma_api_bib_request(bib_ids, alma_marc_xml)
+  def stub_alma_api_bibs_request(bib_ids, alma_marc_xml)
+    bib_ids = Array.wrap(bib_ids).join(',')
     stub_request(
       :get,
       "https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs?expand=p_avail,e_avail&format=json&mms_id=#{bib_ids}"
@@ -24,11 +26,12 @@ module AlmaApiMocks
       .to_return(status: 200, body: alma_marc_xml)
   end
 
-  # @return [WebMock::ResquestStub]
-  def stub_alma_api_bib_not_found
+  # @return [WebMock::RequestStub]
+  def stub_alma_api_bibs_not_found(bib_ids)
+    bib_ids = Array.wrap(bib_ids).join(',')
     stub_request(
       :get,
-      'https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs?expand=p_avail&mms_id=0000000000'
+      "https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs?expand=p_avail,e_avail&format=json&mms_id=#{bib_ids}"
     )
       .to_return(status: 200,
                  body: '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><bibs total_record_count="0"/>')
@@ -44,7 +47,8 @@ module AlmaApiMocks
                  body: 'this is not valid XML')
   end
 
-  # @param [String] bib_ids comma separated mms ids of alma bibliographic record
+  # @param [String] bib_ids list of mms ids of alma bibliographic record
+  # @param [String] body the body of the http response
   # @return [WebMock::RequestStub]
   def stub_alma_api_bib_http_error(bib_ids)
     stub_request(
@@ -54,7 +58,7 @@ module AlmaApiMocks
   end
 
   # @return [String]
-  def api_bib_mock_error
+  def alma_api_bibs_mock_error
     JSON.generate({
                     errorsExist: true,
                     errorList: {
