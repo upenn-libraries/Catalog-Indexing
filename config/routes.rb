03b77c353c99b19f6e398296b637aa2d871ceb89
Amrey Mathurin <mamrey@upenn.edu>
2023-08-09 11:40:39 -0400

    Reflect changes to action name in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root 'alma_indexing#index'
   post 'process', to: 'alma_indexing#process_ids'
   get 'webhook_indexing', to: 'webhook_indexing#challenge'
-  post 'webhook_indexing', to: 'webhook_indexing#listen'
+  post 'webhook_indexing', to: 'webhook_indexing#handle_bib_action'
 end
