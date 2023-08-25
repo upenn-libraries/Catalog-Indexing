9ba94101342e12725d5790c45ad11ee0238e887a
Amrey Mathurin <mamrey@upenn.edu>
2023-08-25 08:07:48 -0400

    Implement Client to interact with SFTP server that stores alma publish files

---
+++ app/services/sftp/client.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require 'net/sftp'
+
+module Sftp
+  # API for interacting with SFTP server that stores Alma publish files
+  class Client
+    HOST = 'move.library.upenn.edu'
+    ROOT = '/recordstoalma/bibexport'
+    class Error < StandardError; end
+
+    # list alma publish files on sftp server
+    # @return [Array<Sftp::PublishFile>] list of PublishFile objects
+    def files
+      @files ||= sftp.dir.glob(ROOT, '*.xml.tar.gz').map do |entry|
+        PublishFile.new(entry.name)
+      end
+    end
+
+    # download single file from sftp server
+    # @param [publish_file] publish_file
+    # @param [TrueClass | FalseClass] wait determines whether to run the event loop, allowing the download to progress
+    # @return [Net::SFTP::Operations::Download]
+    def download(publish_file, wait: true)
+      File.truncate(publish_file.local_path, 0) if publish_file.downloaded?
+      begin
+        download = sftp.download(publish_file.remote_path, publish_file.local_path)
+        return download unless wait
+
+        download.wait
+      rescue Net::SFTP::Exception => e
+        raise Error "Could not download file from sftp server: #{e.message}"
+      end
+    end
+
+    # download all files alma published on sftp server in parallel
+    # @return [Array <Net::SFTP::Operations::Download>]
+    def download_all
+      downloads = files.map { |publish_file| download(publish_file, wait: false) }
+      downloads.each(&:wait)
+    end
+
+    # delete file on sftp server
+    def delete(publish_file)
+      sftp.remove(publish_file.remote_path).wait
+    rescue Net::SFTP::Exception => e
+      raise Error "Could not delete file on sftp server: #{e.message}"
+    end
+
+    def sftp
+      @sftp ||= Net::SFTP.start(HOST, sftp_username, password: sftp_password)
+    rescue Net::SSH::RuntimeError => e
+      raise Error "Could not connect to sftp server: #{e.message}"
+    end
+
+    private
+
+    # @return String
+    def sftp_username
+      @sftp_username ||= Rails.application.credentials.sftp_username
+    end
+
+    # @return String
+    def sftp_password
+      @sftp_password ||= Rails.application.credentials.sftp_password
+    end
+  end
+end
