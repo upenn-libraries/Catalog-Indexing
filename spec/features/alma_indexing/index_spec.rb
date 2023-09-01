0bfa7f80c03a50ba2dc40e77010f414f77fbf971
Patrick Perkins <lilblizzard97@gmail.com>
2023-09-01 17:06:42 -0400

    require capybara, basic tests for alma indexing form

---
+++ spec/features/alma_indexing/index_spec.rb
@@ -0,0 +1,19 @@
+describe 'the alma indexing process' do
+  before do
+    visit index_by_id_path
+  end
+
+  it 'succeeds with valid IDs' do
+    fill_in "MMS IDs", with: '1987342, 1239874'
+    click_on 'Submit'
+
+    expect(find('div.alert')).to have_text 'Indexing job enqueued.'
+  end
+
+  it 'fails with invalid IDs' do
+    fill_in 'MMS IDs', with: 'test, ' * 101
+    click_on 'Submit'
+
+    expect(find('div.alert')).to have_text 'Number of MMS IDs (101) exceeds the limit (100)'
+  end
+end
