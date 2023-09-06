81477b0e8f03542e0f1a2f6cc2ee75b9a657ded4
Mike Kanning <mkanning@upenn.edu>
2023-09-06 11:34:04 -0400

    update SFTP Client to match needs of ProcessPublishJob transaction, update specs and remove unused code

--- app/services/sftp/client.rb
+++ app/services/sftp/client.rb
@@ -7,9 +7,10 @@ module Sftp
   class Client
     HOST = 'move.library.upenn.edu'
     ROOT = '/recordstoalma/bibexport'
+
     class Error < StandardError; end
 
-    attr_accessor :sftp
+    attr_reader :sftp
 
     def initialize
       @sftp = Net::SFTP.start(HOST, sftp_username, password: sftp_password)
@@ -18,7 +19,7 @@ module Sftp
     end
 
     # list files on sftp server that match pattern, returning Sftp::File objects
-    # @param [String] matching prefix to match files in directory
+    # @param [Regexp] matching prefix to match files in directory
     # @return [Array<Sftp::File>] list of Sftp::File objects
     def files(matching:)
       sftp.dir.entries(ROOT).filter_map do |entry|
@@ -56,90 +57,20 @@ module Sftp
 
     # Determine if a file in the SFTP directory is desired in this operation
     # @param [String] file_name
+    # @param [Regexp] matching regex
     # @return [Boolean]
     def desired_file?(file_name, matching)
       file_name.match?(matching) && file_name.ends_with?('.xml.tar.gz')
     end
 
-    # @return String
+    # @return [String]
     def sftp_username
-      @sftp_username ||= Rails.application.credentials.sftp_username
+      Rails.application.credentials.sftp_username
     end
 
-    # @return String
+    # @return [String]
     def sftp_password
-      @sftp_password ||= Rails.application.credentials.sftp_password
-    end
-  end
-
-  # API for interacting with SFTP server that stores Alma publish files
-  class ExpClient
-    HOST = 'move.library.upenn.edu'
-    ROOT = '/recordstoalma/bibexport'
-
-    class Error < StandardError; end
-
-    def self.files(matching:)
-      files = []
-      Net::SFTP.start(HOST, sftp_username, password: sftp_password) do |sftp|
-        sftp.dir.entries(ROOT).each do |file|
-          files << Sftp::File.new(file.name) if desired_file?(file, matching)
-        end
-      end
-      files
-    end
-
-    def self.download(sftp_files)
-      Net::SFTP.start(HOST, sftp_username, password: sftp_password) do |sftp|
-
-      end
-
-      files.each_slice(CONCURRENT_DOWNLOADS) do |slice|
-        sftp_downloads(slice, sftp, completed_proc).each(&:wait)
-      end
-    end
-
-    # @param [Regexp] matching to match interesting files, use date or Job ID
-    # @return [Array<Sftp::File>]
-    def downloads(matching:, completed_proc:)
-      files = []
-      Net::SFTP.start(HOST, sftp_username, password: sftp_password) do |sftp|
-        sftp.dir.entries(ROOT).each do |file|
-          files << Sftp::File.new(file.name) if desired_file?(file, matching)
-        end
-        files.each_slice(CONCURRENT_DOWNLOADS) do |slice|
-          sftp_downloads(slice, sftp, completed_proc).each(&:wait)
-        end
-      end
-      files
-    end
-
-    private
-
-    # @param [Array<Sftp::File>] files
-    # @param [Net::SFTP::Session] sftp
-    # @return [Array<Net::SFTP::Operations::Download>]
-    def sftp_downloads(files, sftp, completed_proc)
-      files.map do |file|
-        ::File.truncate(file.local_path, 0) if ::File.exist?(file.local_path)
-        sftp.download(file.remote_path, file.local_path) do |event, _downloader, *_args|
-          case event
-          when :close
-            puts "File downloaded: #{file.remote_path}"
-            completed_proc.call(file)
-          end
-        end
-      end
-    end
-
-    # @return String
-    def sftp_username
-      @sftp_username ||= Rails.application.credentials.sftp_username
-    end
-
-    # @return String
-    def sftp_password
-      @sftp_password ||= Rails.application.credentials.sftp_password
+      Rails.application.credentials.sftp_password
     end
   end
 end
