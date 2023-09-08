e40e01a6157965fe29440bda06f5efddf80c5200
Mike Kanning <mkanning@upenn.edu>
2023-09-08 15:24:11 -0400

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -38,5 +38,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_07_174713) do
     t.index ["alma_export_id"], name: "index_batch_files_on_alma_export_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.boolean "active", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+  end
+
   add_foreign_key "batch_files", "alma_exports"
 end
