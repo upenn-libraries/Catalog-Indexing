c2e11f1b9cebcb99115c37625f53c390fe88e947
Amrey Mathurin <mamrey@upenn.edu>
2023-09-12 17:47:48 -0400

    Test additional Solr::Admin methods - reset - configset methods - custom error parsing

--- spec/services/solr/admin_spec.rb
+++ spec/services/solr/admin_spec.rb
@@ -1,21 +1,82 @@
 # frozen_string_literal: true
 
+# credit to PSU's Solr::Admin specs
+# https://github.com/psu-libraries/scholarsphere/blob/develop/spec/lib/scholarsphere/solr_admin_spec.rb
 describe Solr::Admin do
   let(:admin) { described_class.new }
+
+  describe '.reset' do
+    let(:stubbed_admin) { instance_spy(described_class) }
+
+    before do
+      allow(described_class).to receive(:new).and_return(stubbed_admin)
+      described_class.reset
+    end
+
+    it 'deletes everything' do
+      expect(stubbed_admin).to have_received(:delete_all_collections)
+      expect(stubbed_admin).to have_received(:delete_all_configsets)
+    end
+
+    it 'recreates the collection' do
+      expect(stubbed_admin).to have_received(:upload_config)
+      expect(stubbed_admin).to have_received(:create_collection)
+    end
+  end
+
+  describe '#zip_file' do
+    it 'returns a zip file that we can read' do
+      result = admin.zip_file
+      contents = result.read
+      expect(contents).to be_a(String)
+      expect(contents).not_to be_empty
+    end
+  end
+
+  describe '#configset_exists?' do
+    it 'returns true if configset exists' do
+      expect(admin).to be_configset_exists
+    end
+
+    it 'returns false if configset exists' do
+      allow(admin).to receive(:config_sets).and_return(%w[_default])
+      expect(admin).not_to be_configset_exists
+    end
+  end
+
+  describe '#delete_configset' do
+    before { allow(admin.config).to receive(:dir).and_return('spec/fixtures/solr') }
+
+    it 'deletes a configset' do
+      expect(admin.delete_configset).to be_nil
+    end
+  end
+
+  describe '#delete_all_configsets' do
+    before do
+      allow(admin).to receive(:config_sets).and_return(%w[sample_configset another_configset _default])
+      allow(admin).to receive(:delete_configset).with('sample_configset').and_return(nil)
+      allow(admin).to receive(:delete_configset).with('another_configset').and_return(nil)
+    end
+
+    it 'deletes all the configsetsexcept _default' do
+      expect(admin.delete_all_configsets).to contain_exactly(nil, nil)
+      expect(admin).to have_received(:delete_configset).twice
+    end
+  end
+
+  describe '#upload_config' do
+    before { allow(admin.config).to receive(:dir).and_return('spec/fixtures/solr') }
+    after { admin.delete_configset }
+
+    it 'uploads a new config' do
+      expect(admin.upload_config).to be_nil
+    end
+  end
 
   describe '#all_collections' do
     it 'returns list of all collections' do
       expect(admin.all_collections).to contain_exactly('catalog-development', 'catalog-test')
-    end
-  end
-
-  describe '#collection_exists?' do
-    it 'returns true when a collection exists' do
-      expect(admin).to be_collection_exists(name: 'catalog-test')
-    end
-
-    it 'returns false when a collection does not exist' do
-      expect(admin).not_to be_collection_exists(name: 'catalog-does-not-exist')
     end
   end
 
@@ -54,12 +115,18 @@ describe Solr::Admin do
     end
   end
 
-  describe '#zip_file' do
-    it 'returns a zip file that we can read' do
-      result = admin.zip_file
-      contents = result.read
-      expect(contents).to be_a(String)
-      expect(contents).not_to be_empty
+  context 'when receiving a bad response' do
+    it 'raises a Solr::Admin::Error with a specific error message if response can be parsed as JSON' do
+      expect { admin.create_collection(name: nil, configset: nil) }.to raise_error(
+        Solr::Admin::Error, /Request to Solr failed with code 400: Invalid collection: \[\]/
+      )
+    end
+
+    it "raises a Solr::Admin::Error with a generic message if response can't be parsed as JSON" do
+      allow(JSON).to receive(:parse).and_raise(JSON::ParserError)
+      expect {
+        admin.create_collection(name: nil, configset: nil)
+      }.to raise_error(Solr::Admin::Error, /Request to Solr failed\./)
     end
   end
 end
