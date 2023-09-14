0f3b19c5c41b0ce636119081f286432442ed034a
Patrick Perkins <lilblizzard97@gmail.com>
2023-09-14 13:26:14 -0400

    rename set to load in batch files controller

--- app/controllers/batch_files_controller.rb
+++ app/controllers/batch_files_controller.rb
@@ -2,8 +2,8 @@
 
 # actions for displaying an AlmaExport's batch files
 class BatchFilesController < ApplicationController
-  before_action :set_alma_export, only: %i[index show]
-  before_action :set_batch_file, only: %i[show]
+  before_action :load_alma_export, only: %i[index show]
+  before_action :load_batch_file, only: %i[show]
 
   def index
     @batch_files = @alma_export.batch_files
@@ -13,11 +13,11 @@ class BatchFilesController < ApplicationController
 
   private
 
-  def set_alma_export
+  def load_alma_export
     @alma_export = AlmaExport.find(params[:alma_export_id])
   end
 
-  def set_batch_file
+  def load_batch_file
     @batch_file = @alma_export.batch_files.find(params[:id])
   end
 end
