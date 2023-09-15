da83246da941ece38bca649ad5885860d99af8e2
Patrick Perkins <lilblizzard97@gmail.com>
2023-09-15 10:46:07 -0400

    expand alma exports index specs

--- spec/features/alma_exports/index_spec.rb
+++ spec/features/alma_exports/index_spec.rb
@@ -13,5 +13,21 @@ describe 'Alma Export Index Page' do
     it 'lists all Alma Exports' do
       expect(page).to have_css('tr.alma-export', count: AlmaExport.count)
     end
+
+    it 'shows IDs' do
+      expect(page).to have_css('th.id', count: AlmaExport.count)
+    end
+
+    it 'shows started_at' do
+      expect(page).to have_css('td.started-at', count: AlmaExport.count)
+    end
+
+    it 'shows status' do
+      expect(page).to have_css('td.status', count: AlmaExport.count)
+    end
+
+    it 'shows batch file count' do
+      expect(page).to have_css('td.batch-files', count: AlmaExport.count)
+    end
   end
 end
