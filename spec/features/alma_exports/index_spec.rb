d51b1618335798ee11034139a08487f0fa58abbc
Patrick Perkins <lilblizzard97@gmail.com>
2023-09-19 15:02:58 -0400

    simplify test with additional values

--- spec/features/alma_exports/index_spec.rb
+++ spec/features/alma_exports/index_spec.rb
@@ -6,35 +6,44 @@ describe 'Alma Export Index Page' do
   before { sign_in user }
 
   context 'when viewing Alma Exports' do
-    let!(:alma_export) { create(:alma_export) }
+    let!(:alma_export) { create(:alma_export, **additional_values) }
+    let(:alma_export_row) { ".alma-export-row.row-id-#{alma_export.id}" }
+    let(:additional_values) { {} }
 
     before { visit alma_exports_path }
 
     it 'lists all Alma Exports' do
-      expect(page).to have_css('tr.alma-export', count: AlmaExport.count)
+      expect(page).to have_css('.alma-export-row', count: AlmaExport.count)
     end
 
     it 'displays IDs' do
-      within('th.id') { expect(page).to have_link(alma_export.id.to_s) }
+      within(alma_export_row) do
+        expect(find('.id')).to have_link(alma_export.id.to_s)
+      end
     end
 
     it 'displays Not Started when started_at is nil' do
-      within('td.started-at') { expect(page).to have_text('Not Started') }
+      within(alma_export_row) do
+        expect(find('.started-at')).to have_text('Not Started')
+      end
     end
 
-    # TODO: implement test for checking started_at when it is populated
-    # it 'displays started_at' do
-    #   within('tr.started-at') do
-    #     expect(page).to have_text(alma_export.started_at)
-    #   end
-    # end
+    context 'with additional values' do
+      let(:additional_values) { { started_at: Time.current } }
+
+      it 'displays started_at' do
+        within('.started-at') do
+          expect(page).to have_text(alma_export.started_at)
+        end
+      end
+    end
 
     it 'displays status' do
-      within('td.status') { expect(page).to have_text(alma_export.status.capitalize) }
+      within('.alma-export-row') { expect(find('.status')).to have_text(alma_export.status.capitalize) }
     end
 
     it 'displays batch file count' do
-      within('td.batch-files') { expect(page).to have_text(alma_export.batch_files.count) }
+      within('.alma-export-row') { expect(find('.batch-files')).to have_text(alma_export.batch_files.count) }
     end
   end
 end
