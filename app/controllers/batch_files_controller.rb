250a8b84eea21757486c10be8bb76b5a3fc3d7c7
Patrick Perkins <lilblizzard97@gmail.com>
2023-10-02 16:59:40 -0400

    batch file scope and controller filters

--- app/controllers/batch_files_controller.rb
+++ app/controllers/batch_files_controller.rb
@@ -7,6 +7,11 @@ class BatchFilesController < ApplicationController
 
   def index
     @batch_files = @alma_export.batch_files.page(params[:page])
+    @batch_files = @batch_files.filter_search(params.dig('filter', 'search')) if params.dig('filter', 'search').present?
+    @batch_files = @batch_files.filter_status(params.dig('filter', 'status')) if params.dig('filter', 'status').present?
+    if params.dig('filter', 'sort_value').present? && params.dig('filter', 'sort_order').present?
+      @batch_files = @batch_files.filter_sort_by(params.dig('filter', 'sort_value'), params.dig('filter', 'sort_order'))
+    end
   end
 
   def show; end
