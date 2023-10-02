3d52fbf2f06267bcea8689d0f49a96be2f0af10c
Amrey Mathurin <mamrey@upenn.edu>
2023-10-02 10:28:18 -0400

    Allow all transactions to use #prepare_marcxml step

---
+++ app/transactions/steps/prepare_marcxml.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Steps
+  # Step to prepare MARCXML for indexing into Solr
+  class PrepareMARCXML
+    include Dry::Monads[:result]
+    # Shove MARCXML into a XML string for a MARCXMLReader to parse into MARC::Record objects
+    # @param [Array|String]  docs
+    # @return [Dry::Monads::Result]
+    def call(docs:, **args)
+      xml = Array.wrap(docs).join.gsub('<?xml version="1.0" encoding="UTF-16"?>', '')
+      io = StringIO.new "<?xml version=\"1.0\" encoding=\"UTF-8\"?><collection>#{xml}</collection>"
+
+      Success(io: io, **args)
+    rescue StandardError => e
+      Failure e
+    end
+  end
+end
