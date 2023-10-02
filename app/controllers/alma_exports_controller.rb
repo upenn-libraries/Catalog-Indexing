f586f0c47f92aa8c8f6a71381e08637916f8dedc
Patrick Perkins <lilblizzard97@gmail.com>
2023-10-02 15:26:53 -0400

    remove todo

--- app/controllers/alma_exports_controller.rb
+++ app/controllers/alma_exports_controller.rb
@@ -5,7 +5,7 @@ class AlmaExportsController < ApplicationController
   before_action :load_alma_export, only: %i[show destroy]
 
   def index
-    @alma_exports = AlmaExport.all.includes(:batch_files).page(params[:page]) # TODO: add sort/filter functionality
+    @alma_exports = AlmaExport.all.includes(:batch_files).page(params[:page])
     @alma_exports = @alma_exports.filter_status(params.dig('filter', 'status')) if params.dig('filter', 'status').present?
     if params.dig('filter', 'sort_value').present? && params.dig('filter', 'sort_order').present?
       @alma_exports = @alma_exports.filter_sort_by(params.dig('filter', 'sort_value'), params.dig('filter', 'sort_order'))
