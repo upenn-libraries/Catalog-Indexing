891cc2a8c88c7e4a8011c9339ba7ed6a990a735d
Patrick Perkins <lilblizzard97@gmail.com>
2023-10-03 13:14:31 -0400

    rename filter helper, return array since hash is unnecessary

--- app/helpers/filter_helper.rb
+++ app/helpers/filter_helper.rb
@@ -3,10 +3,10 @@
 # helper methods for filtering
 module FilterHelper
   # When filtering, we want to display nice values to the user, but submit parseable values to the controller. This
-  # function makes values in an array pretty and maps them to the parseable values in a hash.
-  def titleize_and_hash(options)
-    options.filter_map { |original_string|
-      [original_string.titleize, original_string]
-    }.to_h
+  # function prettifies values in a given array and pairs them with their parseable values.
+  # @param [Array] values
+  # @return [Array]
+  def titleize_and_pair(values)
+    values.filter_map { |original_string| [original_string.titleize, original_string] }
   end
 end
