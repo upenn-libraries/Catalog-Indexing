0c7685d817a1cedf5f8ae33262912e60a33341f5
Mike Kanning <mkanning@upenn.edu>
2023-10-24 11:37:27 -0400

    rename `prepare_writer_and_indexer` method since it primarily works on only the writer. Ensure addition args are passed along in that step.

--- app/transactions/index_by_bib_event.rb
+++ app/transactions/index_by_bib_event.rb
@@ -7,13 +7,13 @@ class IndexByBibEvent
   include Dry::Transaction(container: Container)
 
   step :prepare_marcxml, with: 'prepare_marcxml' # massage MARCXML - for now ensure UTF-8
-  step :prepare_writer_and_indexer
+  step :prepare_writer
   step :index_via_traject, with: 'traject.index_records' # receive a IO object and do the indexing
 
-  def prepare_writer_and_indexer(io:, **args)
+  def prepare_writer(io:, **args)
     target_collections = Array.wrap(Solr::Config.new.collection_name) # TODO: eventually pull this from settings, e.g., Settings.webhook_target_collections
     writer = MultiCollectionWriter.new({ 'solr_writer.target_collections' => target_collections })
-    Success(io: io, writer: writer, indexer: PennMarcIndexer.new)
+    Success(io: io, writer: writer, indexer: PennMarcIndexer.new, **args)
   rescue StandardError => e
     Failure("Problem preparing writer: #{e.message}")
   end
