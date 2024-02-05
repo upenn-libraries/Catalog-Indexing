b64939f00129e9d651d28ff2481b90d461ba2e46
Patrick Perkins <lilblizzard97@gmail.com>
2024-02-05 14:22:03 -0500

    index by bib event job and spec

--- spec/job/index_by_bib_event_job_spec.rb
+++ spec/job/index_by_bib_event_job_spec.rb
@@ -1,23 +1,14 @@
 # frozen_string_literal: true
 
+require_relative 'transaction_job_spec'
+
 describe IndexByBibEventJob do
   include FixtureHelpers
 
   let(:sample_mmsid) { '9979201969103681' }
   let(:marcxml) { marc_fixture sample_mmsid }
-  let(:transaction) { IndexByBibEvent.new }
 
-  describe '#perform' do
-    it 'enqueues the job' do
-      described_class.perform_async(marcxml)
-      expect(described_class).to have_enqueued_sidekiq_job.with(marcxml)
-    end
-
-    it 'calls the transaction with the correct argument' do
-      allow(IndexByBibEvent).to receive(:new).and_return(transaction)
-      allow(transaction).to receive(:call)
-      described_class.perform_inline(marcxml)
-      expect(transaction).to have_received(:call).with(docs: marcxml)
-    end
+  it_behaves_like 'TransactionJob' do
+    let(:args) { [marcxml] }
   end
 end
