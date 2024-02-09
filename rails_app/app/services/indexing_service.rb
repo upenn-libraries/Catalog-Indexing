33e568d17f4b16d14a2cebb08f94cf406a962e3d
Amrey Mathurin <mamrey@upenn.edu>
2024-02-09 13:44:04 -0500

    Address rubocop

--- rails_app/app/services/indexing_service.rb
+++ rails_app/app/services/indexing_service.rb
@@ -67,10 +67,10 @@ class IndexingService
   end
 
   def max_skipped
-    @max_skipped ||= (@indexer.settings[:skipped_record_limit] || 0)
+    @max_skipped ||= @indexer.settings[:skipped_record_limit] || 0
   end
 
   def max_failed
-    @max_failed ||= (@indexer.settings[:failed_record_limit] || 0)
+    @max_failed ||= @indexer.settings[:failed_record_limit] || 0
   end
 end
--- rails_app/spec/shared_examples/alma.rb
+++ rails_app/spec/shared_examples/alma.rb
@@ -5,7 +5,7 @@ shared_context 'with successful Alma request to get XML from IDs' do
   let(:bibnumber) { '9979201969103681' }
 
   before do
-    stub_request(:get, "https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs?expand=p_avail,e_avail&format=json&mms_id=#{bibnumber}").
-      to_return(status: 200, body: "", headers: {})
+    stub_request(:get, 'https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs?expand=p_avail,e_avail&format=json' \
+                        "&mms_id=#{bibnumber}").to_return(status: 200, body: '', headers: {})
   end
 end
