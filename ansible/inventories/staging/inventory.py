21de4ccc6e82d2967230130f7c886b7f533a5881
Andrew Kimball <andrewnk@upenn.edu>
2024-04-09 14:57:13 -0400

    Add staging inventory

---
+++ ansible/inventories/staging/inventory.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+import os
+import json
+
+output = {
+    "_meta": {
+        "hostvars": {
+            "SWARM01": {
+                "ansible_host": os.environ["SWARM01"],
+                "ansible_python_interpreter": "/usr/bin/python3",
+                "swarm_labels": [
+                    "traefik",
+                    "zoonavigator"
+                ]
+            },
+            "SWARM02": {
+                "ansible_host": os.environ["SWARM02"],
+                "ansible_python_interpreter": "/usr/bin/python3",
+                "swarm_labels": [
+                    "catalog_indexing",
+                    "postgres",
+                    "redis",
+                    "sidekiq"
+                ]
+            },
+            "SWARM03": {
+                "ansible_host": os.environ["SWARM03"],
+                "ansible_python_interpreter": "/usr/bin/python3",
+                "swarm_labels": [
+                    "solr-1",
+                    "zookeeper-1"
+                ]
+            },
+            "SWARM04": {
+                "ansible_host": os.environ["SWARM04"],
+                "ansible_python_interpreter": "/usr/bin/python3",
+                "swarm_labels": [
+                    "solr-2",
+                    "zookeeper-2"
+                ]
+            },
+            "SWARM05": {
+                "ansible_host": os.environ["SWARM05"],
+                "ansible_python_interpreter": "/usr/bin/python3",
+                "swarm_labels": [
+                    "solr-3",
+                    "zookeeper-3"
+                ]
+            }
+        }
+    },
+    "all": {
+        "children": [
+            "docker_engine",
+            "docker_swarm_manager",
+            "docker_swarm_worker"
+        ]
+    },
+    "docker_engine": {
+        "children": [
+            "staging"
+        ]
+    },
+    "docker_swarm_manager": {
+        "hosts": [
+            "SWARM01"
+        ]
+    },
+    "docker_swarm_worker": {
+        "hosts": [
+            "SWARM01",
+            "SWARM02",
+            "SWARM03",
+            "SWARM04",
+            "SWARM05"
+        ]
+    },
+    "staging": {
+        "hosts": [
+            "SWARM01",
+            "SWARM02",
+            "SWARM03",
+            "SWARM04",
+            "SWARM05"
+        ]
+    }
+}
+
+print(json.dumps(output))
