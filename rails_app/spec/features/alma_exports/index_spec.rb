f227d6e7fccc26459e889921eafb783479938f9c
Patrick Perkins <pperkins@upenn.edu>
2025-01-29 15:31:55 -0500

    Add test for displaying job_identifier in Alma exports index

--- rails_app/spec/features/alma_exports/index_spec.rb
+++ rails_app/spec/features/alma_exports/index_spec.rb
@@ -29,11 +29,17 @@ describe 'Alma Export Index Page' do
     end
 
     context 'with additional values' do
-      let(:additional_values) { { started_at: Time.current } }
+      let(:additional_values) { { started_at: Time.current, job_identifier: '1234' } }
 
       it 'displays started_at' do
         within('.started-at') do
           expect(page).to have_text(alma_export.started_at.to_fs(:display))
+        end
+      end
+
+      it 'displays job_identifier' do
+        within('.job-id') do
+          expect(page).to have_text(alma_export.job_identifier)
         end
       end
     end
