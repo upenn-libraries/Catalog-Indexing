6211b5e09e5ce188e5870b1a2b1769128ebdad43
Patrick Perkins <pperkins@upenn.edu>
2025-01-30 11:20:38 -0500

    separate job details into a concern

--- rails_app/app/models/alma_export.rb
+++ rails_app/app/models/alma_export.rb
@@ -5,6 +5,7 @@
 # the exported records.
 class AlmaExport < ApplicationRecord
   include Statuses
+  include JobDetails
 
   class InvalidStatusError < StandardError; end
 
@@ -79,64 +80,20 @@ class AlmaExport < ApplicationRecord
             })
   end
 
-  # Start time from webhook body
-  # @return [Time]
-  def job_started_at
-    parse_job_timestamp('start_time')
-  end
-
-  # End time from webhook body
-  # @return [Time]
-  def job_ended_at
-    parse_job_timestamp('end_time')
-  end
-
-  # Duration between job start and end
+  # Class for status badge
   # @return [String]
-  def job_duration
-    return unless job_started_at && job_ended_at
-
-    Time.at(job_ended_at - job_started_at).utc.strftime('%H:%M:%S')
-  end
-
-  # Amount of new records in the job
-  # @return [nil, String]
-  def new_records
-    counter_value('label.new.records')
-  end
-
-  # Amount of updated records in the job
-  # @return [nil, String]
-  def updated_records
-    counter_value('label.updated.records')
-  end
-
-  # Amount of deleted records in the job
-  # @return [nil, String]
-  def deleted_records
-    counter_value('label.deleted.records')
+  def badge_class
+    case status
+    when PENDING
+      'text-bg-secondary'
+    when FAILED
+      'text-bg-danger'
+    when COMPLETED
+      'text-bg-success'
+    end
   end
 
   private
-
-  # Parse the related `value` from the specified key from the `counter` from the webhook body
-  # @return [nil, String]
-  def counter_value(value_key)
-    counter = webhook_body&.dig('job_instance', 'counter')
-    return unless counter
-
-    counter.find { |item| item&.dig('type', 'value') == value_key }&.fetch('value')
-  end
-
-  # Parse a timestamp from the webhook body
-  # @param key [String]
-  # @return [nil, Time]
-  def parse_job_timestamp(key)
-    timestamp = webhook_body&.dig('job_instance', key)
-    return unless timestamp
-
-    Time.zone.parse(timestamp)
-  end
 
   # @param statuses [Array<String>]
   # @return [nil, String]
