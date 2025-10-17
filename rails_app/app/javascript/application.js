7a4b93d6809576ed296089b2d09a784699d4aaf8
Mike Kanning <mkanning@upenn.edu>
2025-10-17 15:59:28 -0400

    remove sprockets, bootstrap, sassc-rails and stimulus-rails gems. add in cssbundling-rails and propshaft gems. adapt rails app to serve bootstrap assets using new setup. remove unused stimulus cruft.

--- rails_app/app/javascript/application.js
+++ rails_app/app/javascript/application.js
@@ -1,5 +1,2 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-
-import "popper"
-import "bootstrap"
-import "controllers"
+import * as bootstrap from "bootstrap"
--- rails_app/config/importmap.rb
+++ rails_app/config/importmap.rb
@@ -1,11 +1,5 @@
-# frozen_string_literal: true
-
 # Pin npm packages by running ./bin/importmap
 
-pin 'application', preload: true
+pin "application"
+pin "bootstrap" # @5.3.8
 
-pin 'bootstrap', to: 'bootstrap.min.js', preload: true
-pin 'popper', to: 'popper.js', preload: true
-pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
-pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
-pin_all_from 'app/javascript/controllers', under: 'controllers'
