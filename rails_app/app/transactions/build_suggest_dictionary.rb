7da4e0b24effc6ab04ce47c9bd6073eabc317ba2
Mike Kanning <mkanning@upenn.edu>
2025-10-29 18:23:19 -0400

    debug

--- rails_app/app/transactions/build_suggest_dictionary.rb
+++ rails_app/app/transactions/build_suggest_dictionary.rb
@@ -50,6 +50,9 @@ class BuildSuggestDictionary
   # @return [Dry::Monads::Result]
   def prepare_solr_suggester_build_url(collection:, suggester:, dictionary:, **args)
     uri = SolrTools.suggester_uri(collection: collection, suggester: suggester, dictionary: dictionary)
+    puts "Suggester URL: #{uri.to_s}"
+    puts "Solr URL from ENV: #{ENV['SOLR_URL']}"
+    puts "Solr URL from config: #{Config.solr.url}"
     Success(url: uri.to_s, **args)
   end
 
